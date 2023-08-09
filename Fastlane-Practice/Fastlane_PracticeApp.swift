80cf7195ded190db04ad6c877206236028cf55dd
Dayeon Moon <0217dayun@naver.com>
2023-08-10 02:47:00 +0900

    연습용 프로젝트 생성

---
+++ Fastlane-Practice/Fastlane_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fastlane_PracticeApp.swift
+//  Fastlane-Practice
+//
+//  Created by 제나 on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct Fastlane_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
